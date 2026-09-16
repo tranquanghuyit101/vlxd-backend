267533338bb61151a2899240900c87d32db5e593
tranquanghuyit101 <tranquanghuyit101@gmail.com>
2026-09-16 16:46:22 +0700

    fix: use IDENTITY for Partner id and remove sensitive mail password

--- src/main/java/org/backend/model/Partner.java
+++ src/main/java/org/backend/model/Partner.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 @Data
 public class Partner {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String phone;
